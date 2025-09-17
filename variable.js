33bbf6b01e0a26dda6d9fde0892ba89221dabb52
RohitMaurya-16 <quickhelp36@gmail.com>
2025-09-17 16:29:19 +0000

    container config done

---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail="quickhelp36@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2  //  not allowed
+
+accountEmail="helpquick36@gmil.com"
+accountPassword="54321"
+accountCity="Lucknow"
+
+/* Prefer not to use var datatype
+because of issue in block scope and Functional Scope */
+
+// You can reserve space for variable without datatype but this is a not good way to write code
+
+console.log(accountId);
+
+console.table([accountId. accountEmail,accountPassword,accountCity, accountState])
